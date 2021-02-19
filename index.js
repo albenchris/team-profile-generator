5abbf5aa4bd1f11c86d8a7b51604ed1f699edc01
albenchris <albenchris00@gmail.com>
2021-02-19 11:05:45 -0600

    answer data passes to generateHTML

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const questions = require('./src/questions');
 const { writeFile, copyFile, generateHTML } = require('./src/generate-page.js');
 const fs = require('fs');
 
-// const employeeList = [];
+let team = []
 
-function promptUser() {
-    let employee = new Employee;
-
+const promptUser = employees => {    
     console.log(`
 ========================
 Add Employee Information
@@ -17,16 +17,40 @@ Add Employee Information
 
     return inquirer
         .prompt(questions)
-        .then((answers) => {
-            employee.name = answers.name;
-            employee.role = answers.role;
-            employee.id = answers.id;
-            employee.email = answers.email;
+        .then((employees) => {   
+            switch (employees.role) {
+                case 'Manager': {
+                    let manager = new Manager();
+                    manager.name = employees.name;
+                    manager.role = employees.role;
+                    manager.id = employees.id;
+                    manager.email = employees.email;
+                    manager.office = employees.office;
+                } 
+                case 'Engineer': {
+                    let engineer = new Engineer();
+                    engineer.name = employees.name;
+                    engineer.role = employees.role;
+                    engineer.id = employees.id;
+                    engineer.email = employees.email;
+                    engineer.github = employees.github;
+                }
+                case 'Intern': {
+                    let intern = new Intern();
+                    intern.name = employees.name;
+                    intern.role = employees.role;
+                    intern.id = employees.id;
+                    intern.email = employees.email;
+                    intern.school = employees.school;
+                }
+            }
 
-            if (answers.continue) {
-                return promptUser(answers);
+            team.push(employees)
+
+            if (employees.continue) {
+                return promptUser(employees);
             } 
-            return employee; 
+            return team; 
         });
 };
 
@@ -34,16 +58,16 @@ promptUser()
     .then(data => {
         return generateHTML(data);
     })
-    .then(pageHTML => {
-        return writeFile(pageHTML);
-    })
-    .then(writeFileResponse => {
-        console.log(writeFileResponse.message);
-        return copyFile();
-    })
-    .then(copyFileResponse => {
-        console.log(copyFileResponse.message);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+    // .then(pageHTML => {
+    //     return writeFile(pageHTML);
+    // })
+    // .then(writeFileResponse => {
+    //     console.log(writeFileResponse.message);
+    //     return copyFile();
+    // })
+    // .then(copyFileResponse => {
+    //     console.log(copyFileResponse.message);
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    // });
--- src/generate-page.js
+++ src/generate-page.js
@@ -33,82 +33,88 @@ const copyFile = () => {
 };
 
 const generateHTML = employeeList => {
-    return `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Team</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Overpass+Mono&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header>
-        <h1>My Team</h1>
-    </header>
+    const [ managers, engineers, interns ] = employeeList
 
-    <main class="container">
-        ${employeeList
-            .filter(({ manager }) => manager)
-            .map(({ name, id, email, office }) => {
-                return `
-        <div class="manager">
-            <h2>${name}</h2>
-            <h3>Manager</h3>
-            <div>
-                <p>ID: ${id}</p>
-                <p>Email: <a href="mailto:${email}">${email}</a></p>
-                <p>Office Number: ${office}</p>
-            </div>
-        </div>
-        `;
-            })
-        .join('')}
+    console.log(managers);
+    console.log(engineers);
+    console.log(interns);
+
+//     return `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>My Team</title>
+//     <link rel="preconnect" href="https://fonts.gstatic.com">
+//     <link href="https://fonts.googleapis.com/css2?family=Overpass+Mono&display=swap" rel="stylesheet">
+//     <link rel="stylesheet" href="style.css">
+// </head>
+// <body>
+//     <header>
+//         <h1>My Team</h1>
+//     </header>
+
+//     <main class="container">
+//         ${managers
+//             // .filter(({ manager }) => manager)
+//             .map(({ name, id, email, office }) => {
+//                 return `
+//         <div class="manager">
+//             <h2>${name}</h2>
+//             <h3>Manager</h3>
+//             <div>
+//                 <p>ID: ${id}</p>
+//                 <p>Email: <a href="mailto:${email}">${email}</a></p>
+//                 <p>Office Number: ${office}</p>
+//             </div>
+//         </div>
+//         `;
+//             })
+//         .join('')}
     
-        ${employeeList
-            .filter(({ engineer }) => engineer)
-            .map(({ name, id, email, github }) => {
-                return `
-        <div class="engineer">
-            <h2>${name}</h2>
-            <h3>Engineer</h3>
-            <div>
-                <p>ID: ${id}</p>
-                <p>Email: <a href="mailto:${email}">${email}</a></p>
-                <p>GitHub: <a href="https://github.com/${github}" target="_blank">${github}</a></p>
-            </div>
-        </div>
-        `;
-            })
-        .join('')}
+//         ${engineers
+//             // .filter(({ engineer }) => engineer)
+//             .map(({ name, id, email, github }) => {
+//                 return `
+//         <div class="engineer">
+//             <h2>${name}</h2>
+//             <h3>Engineer</h3>
+//             <div>
+//                 <p>ID: ${id}</p>
+//                 <p>Email: <a href="mailto:${email}">${email}</a></p>
+//                 <p>GitHub: <a href="https://github.com/${github}" target="_blank">${github}</a></p>
+//             </div>
+//         </div>
+//         `;
+//             })
+//         .join('')}
 
-        ${employeeList
-            .filter(({ intern }) => intern)
-            .map(({ name, id, email, school }) => {
-                return `
-        <div class="intern">
-            <h2>${name}</h2>
-            <h3>Intern</h3>
-            <div>
-                <p>ID: ${id}</p>
-                <p>Email: <a href="mailto:${email}">${email}</a></p>
-                <p>School: ${school}</p>
-            </div>
-        </div>  
-        `;
-            })
-        .join('')}
-    </main>
+//         ${interns
+//             // .filter(({ intern }) => intern)
+//             .map(({ name, id, email, school }) => {
+//                 return `
+//         <div class="intern">
+//             <h2>${name}</h2>
+//             <h3>Intern</h3>
+//             <div>
+//                 <p>ID: ${id}</p>
+//                 <p>Email: <a href="mailto:${email}">${email}</a></p>
+//                 <p>School: ${school}</p>
+//             </div>
+//         </div>  
+//         `;
+//             })
+//         .join('')}
+//     </main>
 
-    <footer>
-        <h4>Made With ☕ by AlBenChris</h4>
-    </footer>
+//     <footer>
+//         <h4>Made With ☕ by AlBenChris</h4>
+//     </footer>
     
-</body>
-</html>
-`;
+// </body>
+// </html>
+// `;
 };
 
 module.exports = { writeFile, copyFile, generateHTML };

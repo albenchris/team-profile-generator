a88682394733bb2f61a69ba2cb714f40ca6be926
albenchris <albenchris00@gmail.com>
2021-02-19 14:45:12 -0600

    changing path for writeFile and copyFile

--- src/generate-page.js
+++ src/generate-page.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 // writeFile promise
 const writeFile = pageHTML => {
     return new Promise((resolve, reject) => {
-        fs.writeFile('./dist/index.html', pageHTML, err => {
+        fs.writeFile('../index.html', pageHTML, err => {
             if (err) {
                 reject(err);
                 return;
@@ -20,7 +20,7 @@ const writeFile = pageHTML => {
 // copyFile promise
 const copyFile = () => {
     return new Promise((resolve, reject) => {
-        fs.copyFile('./src/style.css', './dist/style.css', err => {
+        fs.copyFile('./src/style.css', '../style.css', err => {
             if (err) {
                 reject(err);
                 return;
